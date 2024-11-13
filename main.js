cc16ae35ca2d83dd383fd0cd8d360f9257c38ab1
kaunfell <jenny.kaunistunturi@gmail.com>
2024-11-13 16:31:40 +0200

    small updates

--- main.js
+++ main.js
@@ -29,14 +29,15 @@ document.body.appendChild(musicButton); //this makes the button appear
 
 
 const images = [
-    './resurssit/kuvat/kasvi2.png',
+    //'./resurssit/kuvat/kasvi2.png',
     './resurssit/kuvat/kasvi3.png',
-    './resurssit/kuvat/kasvi4.png',
+    //'./resurssit/kuvat/kasvi4.png',
     './resurssit/kuvat/kasvi5.png',
-    './resurssit/kuvat/kasvi6.png',
+  //  './resurssit/kuvat/kasvi6.png',
     './resurssit/kuvat/kasvi7.png',
     './resurssit/kuvat/kasvi8.png',
     './resurssit/kuvat/kasvi9_anim.gif',
+    './resurssit/kuvat/kasvi_empty.png',
 ]
 
 let currentImageIndex = 0;
@@ -62,12 +63,12 @@ againButton.style.position = 'absolute';
 againButton.style.zIndex = '1';
 againButton.style.top = '36%';
 
-
-let message = document.createElement('p');
-message.className = 'message';
-message.textContent = 'Too much water!';
-message.style.display = 'none';
-plantContainer.appendChild(message);
+const message = document.querySelector(".message");
+//const message = document.createElement("p");
+//message.className = 'message';
+//message.textContent = 'Too much water!';
+//message.style.display = 'none';
+//plantContainer.appendChild(message);
 
 
 let parentContainer = document.querySelector('header');
@@ -90,12 +91,12 @@ plantContainer.addEventListener('drop', function(event) {
             parentContainer.appendChild(againButton);
         }
 
-        if(currentImageIndex > images.length) {
+        if(currentImageIndex >= images.length) {
  
-            message.style.display = 'block';
+            message.style.opacity = '1';
             message.style.position = 'absolute';
             message.style.zIndex = '1';
-            plant.src = '';
+            plant.src= './resurssit/kuvat/kasvi_empty.png';
             parentContainer.appendChild(againButton);
 
         }
@@ -108,72 +109,11 @@ againButton.addEventListener('click', function() {
     plantContainer.style = '';
     currentImageIndex = 0;
     plant.src = initialImageSrc;
-    message.style.display = 'none';
+    message.style.opacity = '0';
     parentContainer.removeChild(againButton);
 });
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-/*const can = document.querySelector('#can');
-const plant = document.querySelector('.kasvi1');
-const plantContainer = document.querySelector('.plantContainer');
-const dragImage = document.querySelector('#dragImage');
-
-let initialPosition = null;
-
-
-
-dragImage.addEventListener('load', function() {
-    can.addEventListener('dragstart', function(event) {
-        event.dataTransfer.setData('text', 'can');
-        initialPosition = { top: can.style.top, left: can.style.left };
-
-        event.dataTransfer.setDragImage(dragImage, 50, 50);
-    });
-});
-
-
-
-//able to drag
-plantContainer.addEventListener('dragover', function(event) {
-    event.preventDefault();
-});
-
-plantContainer.addEventListener('drop', function(event) {
-    event.preventDefault();
-
-    const data = event.dataTransfer.getData('text');
-
-    if(data === 'can') {
-        plant.src='./resurssit/kuvat/kasvi2.png';
-        can.style.top = initialPosition.top;
-        can.style.left = initialPosition.left;
-    }
-});
-
-//dragend
-can.addEventListener('dragend', function(event) {
-    can.style.top = initialPosition.top;
-    can.style.left = initialPosition.left;
-});*/
 
 
 
---
+++ readme.md
@@ -0,0 +1,24 @@
+# 🌸 Water me 🌸
+[originally made 3/2024, small updates 11/2024]
+[comment 11/2024: nowadays I would not approach things like this
+with this structure but it worked then... 😊]
+
+![Water me](./resurssit/kuvat/header.png)
+
+This small game is about: 
+- how to drag things to spesific containers and make it change
+(made with pictures)
+- how to add and toggle
+- create and append elements in JS
+- how to use Font Awesome
+- styling
+- to test pixel sprites
+
+
+
+
+
+🌸 Here is the link to play the game:
+https://kaunfell.github.io/Water-me/
+
+
